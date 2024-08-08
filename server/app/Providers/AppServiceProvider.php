4e2f08258d1750cf243c3c11cde5c7a587e53a8b
Jhefrey Sajot <jsajot@fullscale.io>
2024-08-08 16:59:37 +0800

    log sql queries in local environment

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +25,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (App::environment('local')) {
+            DB::listen(function (QueryExecuted $query) {
+                if (config('app.debug')) {
+                    $sql = Str::replaceArray('?', $query->bindings, $query->sql);
+                    Log::channel('database')->debug($sql);
+                }
+            });
+        }
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
     }
 }
