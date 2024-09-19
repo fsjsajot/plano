03f0d08d33f5bc44868850564751cf83fab7a888
Jhefrey Sajot <jsajot@fullscale.io>
2024-09-19 22:58:49 +0800

    create and setup a new axios instance

--- client/src/lib/http.ts
+++ client/src/lib/http.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const http = axios.create({
-  baseURL: import.meta.env.BASE_URL || "http://localhost:8000",
+export const http = axios.create({
+  baseURL: "http://localhost:8000",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
   withCredentials: true,
   withXSRFToken: true,
 });
-
-export default http;
