e1814039e2de2fa73294616642c167cb8fcca54e
Jhefrey Sajot <jsajot@fullscale.io>
2024-09-19 23:09:18 +0800

    added a schedule for disabling users that are unverified 2 weeks after registration

--- server/routes/console.php
+++ server/routes/console.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// disable user if not verified 14 days after registration
+Schedule::call(function () {
+    User::whereNull('verified_at')
+        ->where('created_at', '<', Carbon::now()->subWeeks(2))
+        ->where('user_role_id', 2)
+        ->update(['disabled_at' => now()]);
+})->daily();
