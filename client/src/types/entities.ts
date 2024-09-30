58dbf9b104b617e342bed62f8577eb3fcc8f6f9f
Jhefrey Sajot <jsajot@fullscale.io>
2024-09-30 22:33:10 +0800

    added model typings

---
+++ client/src/types/entities.ts
@@ -0,0 +1,24 @@
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Workspace {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Board {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+}
