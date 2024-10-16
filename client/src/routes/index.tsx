1738b95cf17358176802edf02e0f6bd4c2b717d1
Jhefrey Sajot <jsajot@fullscale.io>
2024-10-16 16:58:37 +0800

    add workspace invite route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,11 +2,14 @@ import { RouteObject } from "react-router-dom";
 import { authRoutes } from "./auth";
 import { appRoutes } from "./app";
 import RootPage from "@/features/RootPage";
+import { WorkspaceInvite } from "@/features/workspace-invite";
+import { DashboardErrorBoundary } from "@/features/app/dashboard/DashboardErrorBoundary";
 
 export const routes: RouteObject[] = [
   {
     id: "root",
     path: "/",
+    errorElement: <DashboardErrorBoundary/>,
     children: [
       {
         path: "",
@@ -16,6 +19,11 @@ export const routes: RouteObject[] = [
 
       authRoutes,
       ...appRoutes,
+
+      {
+        path: "/invite/:workspaceId/:token",
+        element: <WorkspaceInvite />,
+      },
     ],
   },
 ];
