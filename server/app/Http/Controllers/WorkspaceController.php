221607b2603530839162d6b7fd3bf289314785b5
Jhefrey Sajot <jsajot@fullscale.io>
2024-10-16 17:44:14 +0800

    remove unused imports and formatting

--- server/app/Http/Controllers/WorkspaceController.php
+++ server/app/Http/Controllers/WorkspaceController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\WorkspaceResource;
 use App\Models\Workspace;
-use App\Http\Requests\StoreWorkspaceRequest;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -60,11 +58,11 @@ class WorkspaceController extends Controller
         ]);
 
         $workspace->name = $params["name"];
-        
+
         if ($request->has('description')) {
             $workspace->description = $request->description;
         }
-        
+
         $workspace->save();
 
         return new WorkspaceResource($workspace);
