54a1a0da7c428209b98721d5d35981ca47d1468e
Jhefrey Sajot <jsajot@fullscale.io>
2024-10-16 16:50:40 +0800

    add default react-query config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,17 +3,15 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
+import "@fontsource-variable/inter";
 
 import App from "./App.tsx";
-
-import "@fontsource-variable/inter";
 import "./index.css";
+import { queryConfig } from "./lib/react-query.ts";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
+    ...queryConfig,
   },
 });
 
