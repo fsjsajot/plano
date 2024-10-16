532c80aa1202effec7a88e0d03f59745c2ca7f97
Jhefrey Sajot <jsajot@fullscale.io>
2024-10-16 17:46:15 +0800

    added workspace invite and member routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -8,7 +8,17 @@ use App\Http\Controllers\ItemVoteController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkspaceController;
+use App\Http\Controllers\WorkspaceInviteController;
+use App\Http\Controllers\WorkspaceMemberController;
 use Illuminate\Support\Facades\Route;
+
+// invite by shareable link
+Route::apiResource('workspaces.invites', WorkspaceInviteController::class)->only(['store', 'destroy']);
+Route::get('workspaces/{workspace}/invites/{token?}', [WorkspaceInviteController::class, 'show']);
+Route::post('workspaces/{workspace}/invites/{token}/accept', [WorkspaceInviteController::class, 'acceptInvite']);
+
+// invite by email
+Route::get('workspaces/{workspace}/invited/{token}', [WorkspaceInviteController::class, 'acceptEmailInvite'])->middleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/user', [UserController::class, "show"])->name("current.user");
 
@@ -20,4 +30,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('workspaces.boards.items.comments', ItemCommentController::class)->except('show');
     Route::apiResource('workspaces.boards.items.votes', ItemVoteController::class)->only(['store', 'destroy']);
     Route::apiResource('workspaces.boards.items.files', BoardItemFileController::class)->only(['store', 'destroy', 'index']);
+
+    Route::get('workspaces/{workspace}/members/{memberId}', [WorkspaceMemberController::class, 'show']);
+    Route::apiResource('workspaces.members', WorkspaceMemberController::class)->only(['index', 'destroy']);
 });
