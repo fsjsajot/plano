b5189a8ce102375a2e5f3d9febf9674dbc9cb595
Jhefrey Sajot <jsajot@fullscale.io>
2024-10-16 16:58:10 +0800

    update manage workspace ui styles

--- client/src/features/app/dashboard/manage-workspace.tsx
+++ client/src/features/app/dashboard/manage-workspace.tsx
@@ -19,18 +19,20 @@ export const ManageWorkspace = ({}) => {
 
   return (
     <div className="w-full">
-      <div className="px-8 py-8 ml-10">
-        <h3 className="text-xl font-medium leading-normal tracking-tight">
-          {workspace?.name}
-        </h3>
-        {workspace.description && (
-          <p className="text-sm text-muted-foreground">
-            {workspace.description}
-          </p>
-        )}
+      <div className="px-8 py-8  border-b">
+        <div className="ml-4">
+          <h3 className="text-xl font-medium leading-normal tracking-tight">
+            {workspace?.name}
+          </h3>
+          {workspace.description && (
+            <p className="text-sm text-muted-foreground">
+              {workspace.description}
+            </p>
+          )}
+        </div>
       </div>
 
-      <div className="mx-8 space-y-8">
+      <div className="mx-8 mt-8 space-y-8">
         <EditWorkspaceDetails workspace={workspace} />
         <DeleteWorkspace workspace={workspace} />
       </div>
