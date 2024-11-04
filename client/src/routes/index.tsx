c23c04430bdafd120e27e01ef4a3575b39904a8f
Jhefrey Sajot <jsajot@fullscale.io>
2024-11-04 16:51:29 +0800

    added new routes for new implemented features

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,20 +1,61 @@
-import { RouteObject } from "react-router-dom";
-import { authRoutes } from "./auth";
-import { appRoutes } from "./app";
-import { WorkspaceInvite } from "@/features/workspace-invite/components/workspace-invite";
+import { Outlet, RouteObject } from "react-router-dom";
+
 import { AppErrorBoundary } from "@/components/app-error-boundary/app-error-boundary";
 import { RootLayout } from "@/components/layout/root-layout";
+import { Board } from "@/features/board/components/board";
+import { CreatePost } from "@/features/board/components/create-post";
+import { Home } from "@/features/home/components/home";
+import { HomeContextProvder } from "@/features/home/components/home-context";
+import { EditPost } from "@/features/post/components/edit-post";
+import { Post } from "@/features/post/components/post";
+import { WorkspaceInvite } from "@/features/workspace-invite/components/workspace-invite";
+import { appRoutes } from "./app";
+import { authRoutes } from "./auth";
 
 export const routes: RouteObject[] = [
   {
     id: "root",
     path: "/",
-    errorElement: <AppErrorBoundary/>,
+    errorElement: <AppErrorBoundary />,
     children: [
       {
         path: "",
         index: true,
         element: <RootLayout />,
+      },
+
+      {
+        path: "/app",
+        element: (
+          <HomeContextProvder>
+            <Outlet />
+          </HomeContextProvder>
+        ),
+        children: [
+          {
+            path: ":workspaceId",
+            element: <Home />,
+          },
+
+          {
+            path: ":workspaceId/boards/:boardId",
+            element: <Board />,
+          },
+
+          {
+            path: ":workspaceId/boards/:boardId/new",
+            element: <CreatePost />,
+          },
+
+          {
+            path: ":workspaceId/boards/:boardId/posts/:itemId",
+            element: <Post />,
+          },
+          {
+            path: ":workspaceId/boards/:boardId/posts/:itemId/edit",
+            element: <EditPost />,
+          },
+        ],
       },
 
       authRoutes,
