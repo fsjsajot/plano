b35fd75dd9489df6dcbcd712a5eb51156f96d7b9
Jhefrey Sajot <jsajot@fullscale.io>
2024-11-22 20:11:46 +0800

    added function for updating profile and password

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,4 +17,45 @@ class UserController extends Controller
 
         return new UserResource($userWithWorkspaces);
     }
-}
+
+    public function update_profile(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255'],
+        ]);
+
+        $user = $request->user();
+
+        $user->name = $request->name;
+
+        if ($user->email != $request->email) {
+            $request->validate([
+                'email' => ['unique:' . User::class]
+            ]);
+        }
+
+        $user->email = $request->email;
+        $user->save();
+
+        return new UserResource($user);
+    }
+
+    public function update_password(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', 'string', 'current_password'],
+            'new_password' => ['required', 'string', 'min:8'],
+        ]);
+
+        $user = $request->user();
+        $user->password = Hash::make($request->string('new_password'));
+        $user->save();
+
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+ }
